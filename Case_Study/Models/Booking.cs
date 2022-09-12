cb0b6e5209e3fb6e9e5a1a464677cdf11f14d3f9
ETG1 <ETG1@FNXWSF2-SHEL>
2022-09-12 08:50:34 +0530

    Add project files.

---
+++ Case_Study/Models/Booking.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Case_Study.Models
+{
+
+    
+        [Table("Booking_Details")]
+        public class Booking
+        {
+            [Key]
+            [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+            public int BookingID { get; set; }
+            public int EventID { get; set; }
+            public int UserID { get; set; }
+            public decimal? Booking_Amount { get; set; }
+            public int Booking_User_Id { get; set; }
+            public int Booking_NumberofTickets { get; set; }
+            public decimal? Booking_Single_UnitPrice { get; set; }
+        }
+ }
+
---
+++ Case_Study/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace Case_Study.Models
+{
+    public class User
+    {    
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Mobile { get; set; }
+        public DateTime DOB { get; set; }
+        public string Email { get; set; }
+
+    }
+}
+
